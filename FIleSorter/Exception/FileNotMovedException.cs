42da35d73fecd57ccb528babbb539f7872e600b1
Amrit <Amrit.09@icloud.com>
2023-12-05 14:10:28 -0800

    FileSorter

--- FIleSorter/Exception/FileNotMovedException.cs
+++ FIleSorter/Exception/FileNotMovedException.cs
@@ -8,7 +8,7 @@ namespace FileSorter.Exception
 {
     public class FileNotMovedException : IOException
     {
-        public FileNotMovedException(string filename) : base($"'{filename}': ")
+        public FileNotMovedException(string filename) : base($"'{filename}': An error occurred while moving the file. This error could occur when there are two files of the same name.")
         {
 
         }
